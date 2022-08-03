807860c3aa6b90c01c2b9f9271d8d97ca5e616d4
Bekzat52 <bekzat@bekzat-IdeaPad-3-15ALC6>
2022-08-03 11:31:49 +0600

    like_rating

---
+++ shop/urls.py
@@ -0,0 +1,16 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import ProductViewSet, CategoryViewSet, CommentViewSet, add_rating, toggle_like
+
+
+router = DefaultRouter()
+router.register('products', ProductViewSet)
+router.register('categories', CategoryViewSet)
+router.register('commnets', CommentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('toggle_like/<int:p_id>/', toggle_like),
+    path('add_rating/<int:p_id>/', add_rating),
+
+]
