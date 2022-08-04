4a47d584079eb8582fbc6ef3150d4fd285668efa
Bekzat52 <bekzat@bekzat-IdeaPad-3-15ALC6>
2022-08-04 10:33:34 +0600

    search

--- account/models.py
+++ account/models.py
@@ -1,3 +1,4 @@
+from email import message
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -14,7 +15,9 @@ class UserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.is_active = True
+        user.is_active = False
+        user.send_activation_code()
+
         user.save(using=self._db)
         return user
 
@@ -26,7 +29,6 @@ class UserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        
         user.is_active = True
         user.is_superuser = True
         user.is_staff = True
@@ -38,6 +40,7 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     email = models.EmailField(max_length=150, unique=True)
     username = models.CharField(max_length=150)
+    activation_code = models.CharField(max_length=8, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -46,4 +49,14 @@ class User(AbstractUser):
 
     def generate_activation_code(self):
         from django.utils.crypto import get_random_string
-        
+        code = get_random_string(length=8, allowed_chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+        self.activation_code = code 
+        self.save()
+
+    
+    def send_activation_code(self):
+        from django.core.mail import send_mail
+        self.generate_activation_code()
+        activation_url = f'http://127.0.0.1:8000/account/activate/{self.activation_code}'
+        message = f'Activate your account, following this link {activation_url}'
+        send_mail('Activate account', message, 'shop@gmail.com', [self.email])
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,9 +84,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'py21shop',#название БД(надо создать)
-        'PASSWORD':'1',
-        'USER':'bekzat',
+        'NAME': config('DB_NAME'),#название БД(надо создать)
+        'PASSWORD':config('DB_PASSWORD'),
+        'USER':config('DB_USER'),
         'HOST':'localhost',
         'PORT':'5432'
     }
@@ -155,3 +156,12 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST = 'smtp.gmail.com'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS   = True
+ACTIVATE_USERS_EMAIL = True
+EMAIL_USE_SSL = False
